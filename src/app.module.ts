f36d044b4cff1ab5719f2e12fdb7147339b5cbdf
jos3dduardo <jos3duardolopes@gmail.com>
2025-07-13 22:31:57 -0400

    feat: configure global modules and add file upload support in the application

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,29 @@
 import { Module } from '@nestjs/common';
-import { PlantStatusModule } from './modules/plant-status/plant-status.module';
-import { GrowthStageModule } from './modules/growth-stage/growth-stage.module';
-import { PlantModule } from './modules/plant/plant.module';
-import { UserModule } from './modules/user/user.module';
+import { PlantStatusModule } from '@modules/plant-status/plant-status.module';
+import { GrowthStageModule } from '@modules/growth-stage/growth-stage.module';
+import { PlantModule } from '@modules/plant/plant.module';
+import { UserModule } from '@modules/user/user.module';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { dataSourceOptions, gqlConfig } from '@config/index';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriverConfig } from '@nestjs/apollo';
+import { FileModule } from './modules/file/file.module';
 
 @Module({
-  imports: [PlantStatusModule, GrowthStageModule, PlantModule, UserModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: ['.env'],
+    }),
+    TypeOrmModule.forRoot(dataSourceOptions),
+    GraphQLModule.forRoot<ApolloDriverConfig>(gqlConfig),
+    PlantStatusModule,
+    GrowthStageModule,
+    PlantModule,
+    UserModule,
+    FileModule,
+  ],
   controllers: [],
   providers: [],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { graphqlUploadExpress } from 'graphql-upload-minimal'; // Importe aqui!
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
+  app.use(graphqlUploadExpress({ maxFileSize: 10000000, maxFiles: 10 }));
   await app.listen(process.env.PORT ?? 3000);
 }
 
