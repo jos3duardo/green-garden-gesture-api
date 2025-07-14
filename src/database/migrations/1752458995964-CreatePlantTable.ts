ce7114539d45e41bc63a08002cf5e5b3ece9b32d
jos3dduardo <jos3duardolopes@gmail.com>
2025-07-13 22:29:11 -0400

    feat: add migration to create plants table

---
+++ src/database/migrations/1752458995964-CreatePlantTable.ts
@@ -0,0 +1,88 @@
+import {
+  MigrationInterface,
+  QueryRunner,
+  Table,
+  TableForeignKey,
+} from 'typeorm';
+
+export class CreatePlantTable1752458995964 implements MigrationInterface {
+  private table = new Table({
+    name: 'plants',
+    columns: [
+      {
+        name: 'id',
+        type: 'int',
+        isPrimary: true,
+        isGenerated: true,
+        generationStrategy: 'increment',
+      },
+      {
+        name: 'name',
+        type: 'varchar',
+      },
+      {
+        name: 'type',
+        type: 'varchar',
+      },
+      {
+        name: 'plantedDate',
+        type: 'date',
+      },
+      {
+        name: 'location',
+        type: 'varchar',
+      },
+      {
+        name: 'soilType',
+        type: 'varchar',
+      },
+      {
+        name: 'sunExposure',
+        type: 'varchar',
+      },
+      {
+        name: 'wateringFrequency',
+        type: 'varchar',
+      },
+      {
+        name: 'status',
+        type: 'varchar',
+      },
+      {
+        name: 'file_id',
+        type: 'int',
+      },
+      {
+        name: 'created_at',
+        type: 'timestamp with time zone',
+        default: 'now()',
+      },
+      {
+        name: 'updated_at',
+        type: 'timestamp with time zone',
+        isNullable: true,
+      },
+      {
+        name: 'deleted_at',
+        type: 'timestamp with time zone',
+        isNullable: true,
+      },
+    ],
+  });
+
+  private foreignKey = new TableForeignKey({
+    columnNames: ['file_id'],
+    referencedTableName: 'files',
+    referencedColumnNames: ['id'],
+    onDelete: 'CASCADE',
+  });
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(this.table);
+    await queryRunner.createForeignKeys(this.table.name, [this.foreignKey]);
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable(this.table);
+  }
+}
