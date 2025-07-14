56fc395a71e4b509beb1b813322f9dad7ad35d57
jos3dduardo <jos3duardolopes@gmail.com>
2025-07-13 22:31:30 -0400

    feat: define Plant entity and corresponding GraphQL type for plant data

--- src/modules/plant/entities/plant.entity.ts
+++ src/modules/plant/entities/plant.entity.ts
@@ -1,1 +1,32 @@
-export class Plant {}
+import { Column, Entity } from 'typeorm';
+import { BaseEntity } from '@common/entities';
+
+@Entity('plants')
+export class Plant extends BaseEntity {
+  @Column()
+  name: string;
+
+  @Column()
+  type: string;
+
+  @Column()
+  plantedDate: string;
+
+  @Column()
+  location: string;
+
+  @Column()
+  soilType: string;
+
+  @Column()
+  sunExposure: string;
+
+  @Column()
+  wateringFrequency: string;
+
+  @Column()
+  status: string;
+
+  @Column({ name: 'file_id' })
+  fileId: string;
+}
---
+++ src/modules/plant/entities/plant.type.ts
@@ -0,0 +1,29 @@
+import { Field, ObjectType } from '@nestjs/graphql';
+import { BaseDto } from '@common/entities';
+
+@ObjectType('Plant')
+export class PlantType extends BaseDto {
+  @Field()
+  name: string;
+
+  @Field()
+  type: string;
+
+  @Field()
+  plantedDate: string;
+
+  @Field()
+  location: string;
+
+  @Field()
+  soilType: string;
+
+  @Field()
+  sunExposure: string;
+
+  @Field()
+  wateringFrequency: string;
+
+  @Field()
+  status: string;
+}
