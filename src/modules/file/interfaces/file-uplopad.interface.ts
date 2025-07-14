2871dac3c430bd8e88601fc5415edf9aa09877f8
jos3dduardo <jos3duardolopes@gmail.com>
2025-07-14 00:50:25 -0400

    feat: rename FileUpload interface to FileUploadBuffer and simplify its structure

--- src/modules/file/interfaces/file-uplopad.interface.ts
+++ src/modules/file/interfaces/file-uplopad.interface.ts
@@ -1,19 +1,7 @@
-import type { Readable } from 'node:stream';
-import type { ReadStreamOptions } from 'fs-capacitor';
-
-export interface FileUpload {
+export interface FileUploadBuffer {
+  buffer: Buffer;
   filename: string;
+  fieldName: string;
   mimetype: string;
   encoding: string;
-  // We omit the capacitor property because it's a private implementation detail that shouldn't be used outside.
-  createReadStream: FileUploadCreateReadStream;
-}
-
-export type FileUploadCreateReadStream = (
-  options?: FileUploadCreateReadStreamOptions | undefined,
-) => Readable;
-
-export interface FileUploadCreateReadStreamOptions {
-  encoding?: ReadStreamOptions['encoding'];
-  highWaterMark?: ReadStreamOptions['highWaterMark'];
 }
