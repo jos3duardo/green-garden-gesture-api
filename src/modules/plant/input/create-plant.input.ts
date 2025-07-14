8dca7ecb05fe4e031951555ff24366470eb661dd
jos3dduardo <jos3duardolopes@gmail.com>
2025-07-13 23:59:11 -0400

    feat: enhance CreatePlantInput with additional fields and validation for improved data integrity

--- src/modules/plant/input/create-plant.input.ts
+++ src/modules/plant/input/create-plant.input.ts
@@ -1,7 +1,30 @@
-import { InputType, Int, Field } from '@nestjs/graphql';
+import { Field, InputType } from '@nestjs/graphql';
+import { IsNotEmpty } from 'class-validator';
 
 @InputType()
 export class CreatePlantInput {
-  @Field(() => Int, { description: 'Example field (placeholder)' })
-  exampleField: number;
+  @Field({ description: 'Plant name' })
+  @IsNotEmpty({ message: 'name is required' })
+  name: string;
+
+  @Field({ description: 'Plant type' })
+  type: string;
+
+  @Field({ description: 'Plant planted Date' })
+  plantedDate: Date;
+
+  @Field({ description: 'Plant location' })
+  location: string;
+
+  @Field({ description: 'Plant soil Type' })
+  soilType: string;
+
+  @Field({ description: 'Plant sun Exposure' })
+  sunExposure: string;
+
+  @Field({ description: 'Plant watering Frequency' })
+  wateringFrequency: string;
+
+  @Field({ description: 'Plant status' })
+  status: string;
 }
