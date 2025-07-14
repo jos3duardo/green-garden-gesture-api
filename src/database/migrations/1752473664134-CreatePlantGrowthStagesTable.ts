6763ed977b51998a58a7bfe5382726b53f4eb0c1
jos3dduardo <jos3duardolopes@gmail.com>
2025-07-14 02:45:06 -0400

    feat: create PlantGrowthStages and PlantStatus tables with necessary fields and foreign key relationships

--- src/database/migrations/1752473664134-CreatePlantGrowthStagesTable.ts
+++ src/database/migrations/1752473664134-CreatePlantGrowthStagesTable.ts
@@ -1,11 +1,107 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import {
+  MigrationInterface,
+  QueryRunner,
+  Table,
+  TableForeignKey,
+} from 'typeorm';
 
-export class CreatePlantGrowthStagesTable1752473664134 implements MigrationInterface {
+export class CreatePlantGrowthStagesTable1752473664134
+  implements MigrationInterface
+{
+  private table = new Table({
+    name: 'plants',
+    columns: [
+      {
+        name: 'id',
+        type: 'int',
+        isPrimary: true,
+        isGenerated: true,
+        generationStrategy: 'increment',
+      },
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-    }
+      {
+        name: 'type',
+        type: 'varchar',
+      },
+      {
+        name: 'plantedDate',
+        type: 'date',
+      },
+      {
+        name: 'location',
+        type: 'varchar',
+      },
+      {
+        name: 'soilType',
+        type: 'varchar',
+      },
+      {
+        name: 'sunExposure',
+        type: 'varchar',
+      },
+      {
+        name: 'wateringFrequency',
+        type: 'varchar',
+      },
+      {
+        name: 'status',
+        type: 'varchar',
+      },
+      {
+        name: 'file_id',
+        type: 'int',
+      },
+      {
+        name: 'temperature',
+        type: 'varchar',
+      },
+      {
+        name: 'humidity',
+        type: 'varchar',
+      },
+      {
+        name: 'sunlight',
+        type: 'varchar',
+      },
+      {
+        name: 'description',
+        type: 'text',
+      },
+      {
+        name: 'nextWatering',
+        type: 'varchar',
+      },
+      {
+        name: 'created_at',
+        type: 'timestamp with time zone',
+        default: 'now()',
+      },
+      {
+        name: 'updated_at',
+        type: 'timestamp with time zone',
+        isNullable: true,
+      },
+      {
+        name: 'deleted_at',
+        type: 'timestamp with time zone',
+        isNullable: true,
+      },
+    ],
+  });
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-    }
+  private foreignKey = new TableForeignKey({
+    columnNames: ['file_id'],
+    referencedTableName: 'files',
+    referencedColumnNames: ['id'],
+    onDelete: 'CASCADE',
+  });
 
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(this.table);
+    await queryRunner.createForeignKeys(this.table.name, [this.foreignKey]);
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable(this.table);
+  }
 }
--- src/database/migrations/1752473848953-CreatePlantStatusTable.ts
+++ src/database/migrations/1752473848953-CreatePlantStatusTable.ts
@@ -1,11 +1,43 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
 
 export class CreatePlantStatusTable1752473848953 implements MigrationInterface {
+  private table = new Table({
+    name: 'plant_status',
+    columns: [
+      {
+        name: 'id',
+        type: 'int',
+        isPrimary: true,
+        isGenerated: true,
+        generationStrategy: 'increment',
+      },
+      {
+        name: 'name',
+        type: 'varchar',
+      },
+      {
+        name: 'created_at',
+        type: 'timestamp with time zone',
+        default: 'now()',
+      },
+      {
+        name: 'updated_at',
+        type: 'timestamp with time zone',
+        isNullable: true,
+      },
+      {
+        name: 'deleted_at',
+        type: 'timestamp with time zone',
+        isNullable: true,
+      },
+    ],
+  });
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(this.table);
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable(this.table);
+  }
 }
